7f77dadb0457cc684bcb7ce886235ed815627d35
Iris Foox <57198497+fooxit1239@users.noreply.github.com>
2019-12-01 14:52:07 +0200

    Update ex1.java

--- src/lesson4/ex1.java
+++ src/lesson4/ex1.java
@@ -4,7 +4,6 @@ public class ex1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//יש ליצור מערך ריק בגודל 120 שיכול להכיל מספרים  שלמים. יש למלא מערך במספרים ואז להדפיס סכום חיבור כול המשתנים במערך
          int[] arr=new int[120];
          int i;
          int sum=0;
@@ -15,14 +14,12 @@ public class ex1 {
         
          System.out.println("array's sum is: "+ sum);
          
-         //יש להדפיס סכום של מספרים זוגיים של מערך מתרגיל ראשון
          sum=0;
          for(i=1;i<120;i=+2) {
         	 sum=sum+arr[i];
          }
          System.out.println("array's pairs sum is: "+ sum);
-         
-         // יש להדפיס מספר אי זוגי הכי גדול מתוך מערך של תרגיל ראשון
+		
          int max=0;
          for(i=0;i<120;i=+2) {
         	 if (max<arr[i]) {
