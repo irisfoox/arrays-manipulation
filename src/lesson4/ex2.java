161d52f3441f7e7d65f9d7fdb6733e1a5f39ae1e
Iris Foox <57198497+fooxit1239@users.noreply.github.com>
2019-12-01 14:55:33 +0200

    Update ex2.java

--- src/lesson4/ex2.java
+++ src/lesson4/ex2.java
@@ -4,12 +4,11 @@ public class ex2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        //נתון מערך יכול להכיל שמות של אנשים . תוכן - משה , יוסף , יוסי . יש להדפיס כל השמות בשורה אחת שבין כל שם יהיה רווח וגם פסיק 
-	    String arr[]= {"moshe","yosef","yosi"};
+        
+	    String arr[]= {"michael","daniel","sara"};
 	    String names=String.format("%s ,"+"%s ,"+"%s" , arr[0],arr[1],arr[2]);
 	    System.out.println(names);
 	    
-	    //יש ליצור מערך כולל שמות של מדינות - ישראל ,אוסטריה , ספרד , יש לעשות חיפוש של שם מדינת ישראל , במידה ונמצא יש להדפיס ״המדינה נמצאת במערך״
 	    String states[]= {"israel","austria","spain"};
 	    int i=0;
 	    String n="israel";
